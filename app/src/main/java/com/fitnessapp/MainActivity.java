3b606c007ff0f311a1e308e0ffdb2b3ea410f90c
Arjun <nagarjunaalaparthi@gmail.com>
2018-01-06 13:45:14 +0530

    Adding trainer to the firebase and retrieving trainers in list

--- app/src/main/java/com/fitnessapp/MainActivity.java
+++ app/src/main/java/com/fitnessapp/MainActivity.java
@@ -2,6 +2,16 @@ package com.fitnessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.fitnessapp.data.FirebaseDataManager;
+import com.fitnessapp.data.IDataManager;
+import com.fitnessapp.data.model.Goal;
+import com.fitnessapp.data.model.Trainer;
+
+import java.util.List;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addTrainer();
+
+        FirebaseDataManager.getInstance().loadTrainers(new IDataManager.LoadTrainersCallback() {
+            @Override
+            public void onTrainersLoaded(List<Trainer> trainers) {
+                Toast.makeText(MainActivity.this, "No of trainer : " + trainers.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void addTrainer() {
+        Trainer trainer = new Trainer("Trainer 1", "Running", "500/hr", 4.5, 3);
+
+        FirebaseDataManager.getInstance().addTrainer(trainer, new IDataManager.AddTrainerCallBack() {
+            @Override
+            public void onTrianerAdded() {
+
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+
+        });
     }
 }
--- app/src/main/java/com/fitnessapp/data/FirebaseDataManager.java
+++ app/src/main/java/com/fitnessapp/data/FirebaseDataManager.java
@@ -1,6 +1,20 @@
 package com.fitnessapp.data;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.fitnessapp.data.model.Trainer;
+import com.google.android.gms.location.places.PlaceReport;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by kiran on 1/6/18.
@@ -10,10 +24,16 @@ public class FirebaseDataManager implements IDataManager {
 
   private static volatile IDataManager sDataManager;
 
+  private static DatabaseReference mDatabaseReference;
+
   public static synchronized IDataManager getInstance() {
 
     if (sDataManager == null) {
       sDataManager = new FirebaseDataManager();
+    }
+
+    if (mDatabaseReference == null) {
+      mDatabaseReference = FirebaseDatabase.getInstance().getReference();
     }
     return sDataManager;
   }
@@ -24,5 +44,56 @@ public class FirebaseDataManager implements IDataManager {
 
   @Override public void loadGoal(@NonNull String goalId, @NonNull LoadGoalCallback callback) {
 
+  }
+
+    @Override
+    public void loadTrainers(@NonNull final LoadTrainersCallback callback) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("trainers");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot != null) {
+                    List<Trainer> trainers = new ArrayList<Trainer>();
+                    for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
+                        DataSnapshot snapshot = dataSnapshot.getChildren().iterator().next();
+                        Trainer trainer = snapshot.getValue(Trainer.class);
+                        trainers.add(trainer);
+                    }
+
+                    if (callback != null) {
+                        callback.onTrainersLoaded(trainers);
+                    }
+                 }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+  @Override
+  public void loadTrainer(@NonNull String goalId, @NonNull LoadTrainerCallback callback) {
+
+  }
+
+  @Override
+  public void addTrainer(@NonNull Trainer trainer, @NonNull final AddTrainerCallBack callback) {
+      mDatabaseReference.child("trainers").child(trainer.getId()).setValue(trainer, new DatabaseReference.CompletionListener() {
+        @Override
+        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+
+            if (callback != null) {
+                if (databaseError != null){
+                    callback.onError(databaseError.getMessage());
+                } else {
+                    callback.onTrianerAdded();
+                }
+            }
+        }
+      });
   }
 }
